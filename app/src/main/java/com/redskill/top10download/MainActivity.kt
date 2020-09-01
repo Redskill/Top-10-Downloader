e2360e60b8d9bd1d339d79bafa4ee050e5e83268
Fabio Arosio <fabio_arosio@hotmail.it>
2020-09-01 17:31:51 +0100

    Initial commit

---
+++ app/src/main/java/com/redskill/top10download/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.redskill.top10download
+
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.redskill.top10download.utils.ParseApplications
+import com.redskill.top10download.view.FeedAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
+import kotlin.properties.Delegates
+
+class FeedEntry {
+
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            summary = $summary
+            imageURL = $imageURL
+            """.trimIndent()
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+
+    private var downloadData : DownloadData? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+    }
+
+    private fun downloadUrl(feedUrl: String) {
+        downloadData = DownloadData(this,xmlListView)
+        downloadData?.execute(feedUrl)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feeds_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val feedUrl: String
+
+        when (item?.itemId) {
+            R.id.mnuFree -> feedUrl =
+                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
+            R.id.mnuPaid -> feedUrl =
+                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml"
+            R.id.mnuSongs -> feedUrl =
+                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml"
+            else -> return super.onOptionsItemSelected(item)
+        }
+        downloadUrl(feedUrl)
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        downloadData?.cancel(true)
+    }
+
+    companion object {
+        private class DownloadData(context: Context, listView: ListView) :
+            AsyncTask<String, Void, String>() {
+
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
+
+            override fun doInBackground(vararg url: String?): String {
+                return downloadXML(url[0])
+            }
+
+            override fun onPostExecute(result: String) {
+                super.onPostExecute(result)
+                val parseApplications = ParseApplications()
+                parseApplications.parse(result)
+
+                val feedAdapter =
+                    FeedAdapter(propContext, R.layout.list_record, parseApplications.applications)
+                propListView.adapter = feedAdapter
+            }
+
+            private fun downloadXML(urlPath: String?): String {
+                return URL(urlPath).readText()
+            }
+        }
+    }
+}
